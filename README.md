92b3176b1fdbb7ab5028187646b1c2373521cd88
bhabani sankar sahoo <156910182+bhabanisipu@users.noreply.github.com>
2025-08-17 19:53:35 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# my myntra review SCrapper project
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='Scrapper',
+    version='0.0.1',
+    author='bhabani',
+    author_email='md.a@pw.live',
+    packages=find_packages(),
+    install_requires = []
+)
