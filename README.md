fb04f4b8791292c3815633c0595d4fbefe253941
unknown <sipubhabani63@gmail.com>
2025-10-02 12:47:29 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# myntra review project
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='scrapper',
+    version='0.0.1',
+    author='bhabani',
+    author_email='bhabanisankars136@gmail.com',
+    packages=find_packages(),
+    install_requires = []
+)
